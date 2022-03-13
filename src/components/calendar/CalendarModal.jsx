a51dde749670a023f81167ebd3e16542f170b460
Giuseppe <giuseppe19.genovez@gmail.com>
2022-03-13 12:07:10 -0500

    Adding validation in form calendar

--- src/components/calendar/CalendarModal.jsx
+++ src/components/calendar/CalendarModal.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import moment from "moment";
 import Modal from "react-modal";
 import DateTimePicker from "react-datetime-picker";
+import Swal from "sweetalert2";
 
 const customStyles = {
   content: {
@@ -21,14 +22,55 @@ const nowPlus = now.clone().add(1, "hours");
 export const CalendarModal = () => {
   const [startDate, setStartDate] = useState(now.toDate());
   const [endDate, setEndDate] = useState(nowPlus.toDate());
+  const [titleValid, setTitleValid] = useState(true);
+
+  const [formValues, setFormValues] = useState({
+    title: "Evento",
+    notes: "",
+    start: now.toDate(),
+    end: nowPlus.toDate(),
+  });
+
+  const { notes, title, start, end } = formValues;
+
+  const handleInputChange = ({ target }) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value,
+    });
+  };
+
   const closeModal = () => {};
 
   const startDateChange = (e) => {
     setStartDate(e);
+    setFormValues({
+      ...formValues,
+      start: e,
+    });
   };
 
   const endDateChange = (e) => {
     setEndDate(e);
+    setFormValues({
+      ...formValues,
+      end: e,
+    });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const momentStart = moment(start);
+    const momentEnd = moment(end);
+    if (momentStart.isSameOrAfter(momentEnd)) {
+      return Swal.fire("Error", "Las fechas no deber ser iguales", "error");
+    }
+    if (title.trim().length < 2) {
+      return setTitleValid(false);
+    }
+    // Realizar las acciones a la BD
+    setTitleValid(true);
+    closeModal();
   };
 
   return (
@@ -44,9 +86,14 @@ export const CalendarModal = () => {
       >
         <h1> Nuevo evento </h1>
         <hr />
-        <form className="container">
+        <form onSubmit={submitForm} className="container">
           <div className="mb-3">
-            <label className="form-label">Fecha y hora inicio</label>
+            <label className="form-label">
+              Fecha y hora inicio{" "}
+              <span className="form-text">
+                La hora se cambia con las flechas direccionales
+              </span>
+            </label>
             <DateTimePicker
               onChange={startDateChange}
               value={startDate}
@@ -68,10 +115,12 @@ export const CalendarModal = () => {
             <label className="form-label">Titulo y notas</label>
             <input
               type="text"
-              className="form-control"
+              className={`form-control ${!titleValid && "is-invalid"}`}
               placeholder="Título del evento"
               name="title"
               autoComplete="off"
+              value={title}
+              onChange={handleInputChange}
             />
             <small id="emailHelp" className="form-text">
               Una descripción corta
@@ -84,6 +133,8 @@ export const CalendarModal = () => {
               placeholder="Notas"
               rows="5"
               name="notes"
+              value={notes}
+              onChange={handleInputChange}
             ></textarea>
             <small id="emailHelp" className="form-text">
               Información adicional
